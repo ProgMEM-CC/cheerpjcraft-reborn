dedb7dad8704ed6af502127ffe2086063a73781f
ProgMEMCC <progmem.cc@gmail.com>
2025-07-20 12:43:56 +0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,23 +3,24 @@
 	import GameDisplay from '$lib/GameDisplay.svelte';
 </script>
 
-<style>
-	html, body {
-		margin: 0;
-		padding: 0;
-		height: 100%;
-		width: 100%;
-		overflow: hidden;
-	}
+<svelte:head>
+	<style>
+		html, body, #svelte, #sveltekit-body {
+			width: 100%;
+			height: 100%;
+			margin: 0;
+			padding: 0;
+			overflow: hidden;
+		}
+	</style>
+</svelte:head>
 
+<style>
 	.fullscreen-container {
 		position: fixed;
-		top: 0;
-		left: 0;
+		inset: 0;
 		width: 100vw;
 		height: 100vh;
-		margin: 0;
-		padding: 0;
 		background: black;
 		display: flex;
 		align-items: center;
